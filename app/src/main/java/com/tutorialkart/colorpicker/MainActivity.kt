9ed7c125610aa8f06f591e18920696dd43930dcc
Pedro Lucas <plsb802@gmail.com>
2022-01-03 19:30:44 -0300

    Color Picker App

---
+++ app/src/main/java/com/tutorialkart/colorpicker/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.tutorialkart.colorpicker
+
+import android.app.Activity
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
+import android.text.Editable
+import android.text.TextWatcher
+
+
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        strColor.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {
+                if (s.length == 6){
+                    colorR.progress = Integer.parseInt(s.substring(0..1), 16)
+                    colorG.progress = Integer.parseInt(s.substring(2..3), 16)
+                    colorB.progress = Integer.parseInt(s.substring(4..5), 16)
+                } else if (s.length == 8){
+                    colorR.progress = Integer.parseInt(s.substring(2..3), 16)
+                    colorG.progress = Integer.parseInt(s.substring(4..5), 16)
+                    colorB.progress = Integer.parseInt(s.substring(6..7), 16)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+
+            }
+        })
+
+
+
+
+        colorR.max = 255
+        colorR.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int,
+                                           fromUser: Boolean) {
+                val colorStr = getColorString()
+                strColor.setText(colorStr.replace("#","").toUpperCase())
+                btnColorPreview.setBackgroundColor(Color.parseColor(colorStr))
+            }
+        })
+
+        colorG.max = 255
+        colorG.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int,
+                                           fromUser: Boolean) {
+                val colorStr = getColorString()
+                strColor.setText(colorStr.replace("#","").toUpperCase())
+                btnColorPreview.setBackgroundColor(Color.parseColor(colorStr))
+            }
+        })
+
+        colorB.max = 255
+        colorB.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int,
+                                           fromUser: Boolean) {
+                val colorStr = getColorString()
+                strColor.setText(colorStr.replace("#","").toUpperCase())
+                btnColorPreview.setBackgroundColor(Color.parseColor(colorStr))
+            }
+        })
+
+    }
+    fun getColorString(): String {
+        var r = Integer.toHexString(((255*colorR.progress)/colorR.max))
+        if(r.length==1) r = "0"+r
+        var g = Integer.toHexString(((255*colorG.progress)/colorG.max))
+        if(g.length==1) g = "0"+g
+        var b = Integer.toHexString(((255*colorB.progress)/colorB.max))
+        if(b.length==1) b = "0"+b
+        return "#"  + r + g + b
+    }
+}
